20d9d79d42a90fb04fa7bf2d4482097ca7a7898c
kemmboss <arhamabeerahmed@gmail.com>
2023-05-27 19:36:58 +0500

    react router integrated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import AppRouter from "../src/config/router/index.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>1st edit </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <AppRouter />
+      </div>
+    );
+  }
 }
 
 export default App;
