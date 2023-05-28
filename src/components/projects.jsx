89df8e892a354dafd685d85de263094b709c5c4d
kemmboss <arhamabeerahmed@gmail.com>
2023-05-28 17:25:33 +0500

    project card initially done

---
+++ src/components/projects.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProjectCard from "./projectCard";
+
+function Projects() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <ProjectCard />
+    </div>
+  );
+}
+
+export default Projects;
