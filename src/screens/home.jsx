d67c0fb203f68e2346664158da094b91133ab5d0
kemmboss <arhamabeerahmed@gmail.com>
2023-05-28 17:40:58 +0500

    project initial design

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import LandingPage from "../components/landingPage";
+import Projects from "../components/projects";
 
 function Home() {
   return (
     <div>
       <LandingPage />
+      <Projects />
     </div>
   );
 }
