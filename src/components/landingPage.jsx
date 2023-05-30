afca5c4ee104b551e0f0274102429f0fabb0f572
kemmboss <arhamabeerahmed@gmail.com>
2023-05-30 23:53:55 +0500

    profile pic added

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import back from "../assets/code.png";
+import aaa from "../assets/aaa-black.png";
 import styles from "./component.module.scss";
 import { AiOutlineArrowRight } from "react-icons/ai";
 
@@ -11,7 +11,7 @@ function LandingPage() {
       <div className="overflow-hidden h-[80vh]">
         <div className={styles.curveBG} />
         <div className={styles.landingPic + " fixed w-2/6"}>
-          {/* <img src={back} alt="photo" /> */}
+          <img src={aaa} alt="photo" className="h-full" />
         </div>
         <div className="relative top-28 left-[45%] text-white">
           <span className="ml-[-40px] font-black  text-3xl text-[#f74b07]">
