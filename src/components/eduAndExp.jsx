ab03d65f977fef92010c013762bd42cf0fabca51
kemmboss <arhamabeerahmed@gmail.com>
2023-06-11 16:21:23 +0500

    experience & edu mobile responsive

--- src/components/eduAndExp.jsx
+++ src/components/eduAndExp.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import EduAndExpCard from "./eduAndExpCard";
+import styles from "./component.module.scss";
 
 const edu = [
   {
@@ -41,13 +42,16 @@ function EduAndExp() {
           EXPERIENCES & EDUCATION
         </h1>
       </div>
-      <div className="text-white flex mx-2 px-28 justify-between">
-        <div className=" w-[50%]">
+      <div className="text-white flex max-sm:flex-col mx-2 px-28 max-sm:px-6 justify-between">
+        <div className=" w-[50%] max-sm:w-full">
           {exp.map((experience) => (
             <EduAndExpCard experience={experience} key={experience.company} />
           ))}
         </div>{" "}
-        <div className=" w-[50%]">
+        <hr
+          className={"max-sm:flex max-sm:my-2 flex-none " + styles.seperator}
+        />
+        <div className=" w-[50%] max-sm:w-full">
           {edu.map((experience) => (
             <EduAndExpCard experience={experience} key={experience.company} />
           ))}
